1118d3fda846aebc01b9d4678f87b7813202d4ea
Jan Grodecki <jan.grodecki@rws.com>
2021-01-25 07:19:24 -0800

    Update from Kamal

---
+++ Converter2/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Converter2
+//
+//  Created by Kamal Thakur on 12/01/21.
+//
+
+import Foundation
+
+// localization keys
+enum localizedKeys: String {
+    case answerInCelsius = "answer_in_celsius"
+    case convertToFahrenheit = "convert_to_fahrenheit"
+}
+
--- Converter2/ViewController.swift
+++ Converter2/ViewController.swift
@@ -11,24 +11,35 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var entryTempTextField: UITextField!
+    @IBOutlet weak var btnConvert: UIButton!
     @IBOutlet weak var answerLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        configureLocalize()
     }
 
+    func configureLocalize(){
+        let answer = NSLocalizedString(localizedKeys.answerInCelsius.rawValue, comment: "")
+        let convert = NSLocalizedString(localizedKeys.convertToFahrenheit.rawValue, comment: "")
+        
+        answerLabel.text = answer
+        btnConvert.setTitle(convert, for: .normal)
+    }
+    
     @IBAction func convertButtonClicked(_ sender: UIButton) {
-        if let result = entryTempTextField.text {
-            if(result == "") {
-                return
-            }
-            else {
+        // you can check empty string using ** result.isEmpty ** if it is true means empty/blank string that's why commented below if condition.
+        if let result = entryTempTextField.text, !(result.isEmpty) {
+//            if (result == "") {
+//                return
+//            }
+//            else {
                 if let num = Double(result){
                     let output = num * (9/5) + 32
                     answerLabel.text = String(output)
                 }
-            }
+//            }
         }
     }
     override func didReceiveMemoryWarning() {
